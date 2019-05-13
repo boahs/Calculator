0871d8fd8cad1d2e68cd03ebc7596064d99ae0be
boahs <39522778+boahs@users.noreply.github.com>
2019-05-13 16:49:11 -0400

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter any number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter any number again: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            Console.ReadLine();
+        }
+
+         
+    }
+}
